9b8a753a2acdf3428341ffd8a28136cd014f70f2
shashiwalker <skssunny30@gmail.com>
2020-06-19 16:16:57 +0530

    HackerRank problem: Pairs(easy)

---
+++ HackerRank/Pairs.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the pairs function below.
+def pairs(k, arr):
+
+    arr_set = set(arr)
+    pair_set = set([x+k for x in arr])
+    
+    return len(arr_set.intersection(pair_set))
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
