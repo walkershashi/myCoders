a590c8ae825a364a9af31e773c039f6db1caf63a
shashiwalker <skssunny30@gmail.com>
2020-07-04 13:05:41 +0530

    Leet Code Problem: Remove Element

---
+++ leetCode/removeElements.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        for i in range(nums.count(val)):
+            nums.remove(val)
+        
+        return len(nums)
